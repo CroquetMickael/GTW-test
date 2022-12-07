72abf60ba91067dccdd0c8778976b8f559be37ef
Croquet Mickael <mickael.croquet@laposte.net>
2022-12-07 18:05:40 +0100

    feat(Todo): Ajout du controller Todo avec les méthodes d'ajout, modification, suppression

---
+++ Back/app/Http/Controllers/Api/TodoController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+
+        return response()->json([
+            'todos' => $todos
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $todo = Todo::create($request->all());
+
+        return response()->json([
+            "message" => "Todo created successfully!",
+            'todo'=>$todo
+        ], 200);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $todo = Todo::update($request->all());
+
+        return response()->json([
+            "message"=> "Todo updated successfully",
+            'todo'=>$todo
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response()->json([
+            'message'=> 'Todo deleted sucessfully!'
+        ], 200);
+    }
+}
