f47eaca318df14cbb96dc0ef544157f817f70b0e
Croquet Mickael <mickael.croquet@laposte.net>
2022-12-07 20:20:02 +0100

    feat(Auth): Ajout du controller d'authentification & blocage des routes des todos

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TodoController;
@@ -19,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('todos', TodoController::class);
+Route::apiResource('todos', TodoController::class)->middleware('auth:sanctum');
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
