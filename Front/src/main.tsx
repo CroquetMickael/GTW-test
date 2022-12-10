dd6545788dfd717c26dc67cf19f7e4c898488a14
Croquet Mickael <mickael.croquet@laposte.net>
2022-12-10 13:57:53 +0100

    feat(Main): Ajout du router et des trois routes

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Todos } from "./Pages/Todos/Todos";
+import { Login } from "./Pages/Login";
+import { AuthProvider } from "./context/AuthProvider";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { Register } from "./Pages/Register";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <div className="min-w-screen min-h-screen dark:bg-gray-800 flex items-center justify-center px-5 py-5">
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route index element={<Todos />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+        <ToastContainer />
+      </AuthProvider>
+    </BrowserRouter>
+  </div>
+);
